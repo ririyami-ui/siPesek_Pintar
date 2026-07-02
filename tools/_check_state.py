5a60023fab9be6d5b06b63724ef820e598aea0f0
ririyami-ui <ri2ami77@gmail.com>
2026-07-03 01:04:36 +0700

    chore: remove large zip and temp files from repo

--- tools/_check_state.py
+++
@@ -1,8 +0,0 @@
-import json
-with open('resources/js/utils/bskap_2025_intel.json', 'r', encoding='utf-8') as f:
-    d = json.load(f)
-for g in ['7','8','9']:
-    info = d['subjects']['SMP'][g].get('Informatika')
-    if info:
-        print(f'SMP {g} ganjil elemen: {info["ganjil"]["elemen"]}')
-        print(f'SMP {g} genap elemen: {info["genap"]["elemen"]}')
